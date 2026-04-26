c2731dc471d776981b42a79f481a4afe9f100c7e
Dirk Clemens <git@adcore.de>
2026-04-26 12:33:30 +0200

    Initial Commit

---
+++ exifRenamer/exifRenamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  exifRenamerApp.swift
+//  exifRenamer
+//
+//  Created by Dirk Clemens on 26.04.26.
+//
+
+import SwiftUI
+
+@main
+struct exifRenamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
